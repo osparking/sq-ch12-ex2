07d95de266e37c38063b2de5f1ca6b1e5e97b7d7
osparking <jbpark03@gmail.com>
2023-10-30 20:13:06 +0900

    web, lombok: 두 종속성.

---
+++ src/main/java/sqch12ex1/SqCh12Ex2Application.java
@@ -0,0 +1,13 @@
+package sqch12ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqCh12Ex2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqCh12Ex2Application.class, args);
+	}
+
+}
---
+++ src/test/java/sqch12ex1/SqCh12Ex2ApplicationTests.java
@@ -0,0 +1,13 @@
+package sqch12ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqCh12Ex2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
